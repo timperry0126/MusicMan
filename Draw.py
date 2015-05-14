36a9f7e66bc9a4ae2df99868287d54650850a9f9
tjcombo4life <tperry0126@gmail.com>
2015-05-14 15:58:55 -0700

    Update Draw.py

--- Draw.py
+++ Draw.py
@@ -10,10 +10,10 @@ LEVEL_ONE = 351
 LEVEL_TWO = 238
 LEVEL_THREE = 114
 
-class Player(object):
+class Player(object):   #Player class written by Tim
 
-    def __init__(self): #Constructor
-        self.image = pygame.image.load("Eye_walk_1.png").convert()  #Loads Character
+    def __init__(self, option): #Constructor
+        self.image = pygame.image.load(option + "1.png").convert()  #Loads Character
         self.image.set_colorkey(WHITE)  #Sets color to turn transparent
         self.x = 140 # x position object is drawn at
         self.y = 300 # y position object is drawn at
@@ -79,45 +79,87 @@ class Player(object):
     def draw(self, surface):
         surface.blit(self.image, (self.x, self.y)) #draws player to screen
 
-    def update(self, mod):
+    def update(self, mod, option):
         if (self.stat < self.walk * 2):    #draws first animation
-            self.image = pygame.image.load("Eye_walk_1.png").convert()
+            self.image = pygame.image.load(option + "1.png").convert()
             self.image.set_colorkey(WHITE)
             
         if (self.stat >= self.walk * 2 and self.stat < self.walk * 4):   #draws second animation
-            self.image = pygame.image.load("Eye_walk_2.png").convert()
+            self.image = pygame.image.load(option + "2.png").convert()
             self.image.set_colorkey(WHITE)
             
         if (self.stat >= self.walk * 4 and self.stat < self.walk * 6):  #draws third animation
-            self.image = pygame.image.load("Eye_walk_3.png").convert()
+            self.image = pygame.image.load(option + "3.png").convert()
             self.image.set_colorkey(WHITE)
             
         if (self.stat >= self.walk * 6 and self.stat < self.walk * 8):  #draws fourth animation
-            self.image = pygame.image.load("Eye_walk_4.png").convert()
+            self.image = pygame.image.load(option + "4.png").convert()
             self.image.set_colorkey(WHITE)
 
         if (self.stat >= self.walk * 8 and self.stat < self.walk * 10):  #draws fourth animation
-            self.image = pygame.image.load("Eye_walk_5.png").convert()
+            self.image = pygame.image.load(option + "5.png").convert()
             self.image.set_colorkey(WHITE)
 
         if (self.stat >= self.walk * 10 and self.stat < self.walk * 12):  #draws fourth animation
-            self.image = pygame.image.load("Eye_walk_6.png").convert()
+            self.image = pygame.image.load(option + "6.png").convert()
             self.image.set_colorkey(WHITE)
 
         if (self.stat >= self.walk * 12 and self.stat < self.walk * 14):  #draws fourth animation
-            self.image = pygame.image.load("Eye_walk_7.png").convert()
+            self.image = pygame.image.load(option + "7.png").convert()
             self.image.set_colorkey(WHITE)
 
         if (self.stat >= self.walk * 14 and self.stat < self.walk * 16):  #draws fourth animation
-            self.image = pygame.image.load("Eye_walk_8.png").convert()
+            self.image = pygame.image.load(option + "8.png").convert()
             self.image.set_colorkey(WHITE)
 
-        self.stat += 5 + mod[0] #how fast animation moves plus speed of character
+        if(self.isJump != True): #prevents animation from changing while jumping
+            self.stat += 5 + mod[0] #how fast animation moves plus speed of character
         
         if (self.stat > self.walk * 16):   #loops back to beginning animation
             self.stat = 1
 
-class Platform(object):
+
+#---Sound Controls------------------------Paul----
+#Func Desc.-------------------------
+#The following fuctions would have controlled sound for the game. It would take
+#the players y axis and compare it to the y-values of the platforms. So
+#depending on the character and the platform there would be a different song.
+#---Problem
+#The issue I am facing with this function is getting the screen to
+#update when it was playing music. It will either play music and freeze the screen
+#or not play music until the screen froze.
+    #def platformSound(self, choice):
+        #while pygame.mixer.get_busy() == False:
+            #if self.y >= 351:
+                #pygame.mixer.music.load(choice + '1.wav')
+                #pygame.mixer.music.play(-1)
+            
+            #if self.y < 351 and self.y >= 238
+                #pygame.mixer.music.load(choice + '2.wav')
+                #pygame.mixer.music.play(-1)
+            #if self.y <238
+                #pygame.mixer.music.load(choice + '3.wav')
+                #pygame.mixer.music.play(-1)
+
+#Func. Desc.------------------------------
+#This function will pick and play a random sound from the predetermined list. This
+#function uses the random.choice function built into pygame.
+#Problem----------------------------------
+#I am not sure the issue with this function it would cause both the shell and IDLE
+#crash when running. I was able to get a simplified version of the function to work
+#although it would have the same problem with the other function and would freeze the screen.
+            
+    #def play_sound():
+        #next_sound = random.choice(sound_Effects)
+        #while next_sound == currently_playing_sound:
+           # next_song = random.choice(sound_Effects)
+        #currently_playing_sound = next_sound
+        #pygame.mixer.music.load(next_sound)
+        #pygame.mixer.music.play()
+        #if pygame.mixer.music.get_busy() == False:
+            #pygame.mixer.music.stop()
+
+class Platform(object):     #Platform class written by Tim
     def __init__(self, xPos, yPos, image, h, l):
         self.x = xPos # x position of platform
         self.y = yPos # y position of platform
@@ -192,3 +234,5 @@ class CharacterSelector(object):
         
         if (self.stat > self.walk * 16):   #loops back to beginning animation
             self.stat = 1
+
+
